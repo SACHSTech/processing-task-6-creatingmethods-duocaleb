2e0023400cf3839aeea8e82681c2176c9d981b5c
caleb <duocaleb3@gmail.com>
2024-04-17 12:21:39 -0400

    Done

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,13 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+	int screenSize = 400;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(screenSize, screenSize);
   }
 
   /** 
@@ -23,14 +23,59 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  for(int x = 0; x < 9; x++){
+      for(int y = 0; y < 9; y++){
+        drawFlower(8, 100, -200+50*x, -200+50*y);
+      }
+    }
   }
-  
   // define other methods down here.
+  public void drawFlower(int intPetals, int intSize, int intLocationX, int intLocationY) {
+    if (intPetals >= 30){
+      strokeWeight(0);
+    }
+    else{
+      strokeWeight(1);
+    }
+
+    //Main drawing loop
+    for (int x = 0; x < 360; x += 360/intPetals){
+      pushMatrix();
+
+      //making a smooth gradient of colours
+      if (x*intPetals/360 < intPetals/2){
+        fill(0,255-(17/12*x),x*17/12);
+      }
+      else{
+        fill(0 , 255-(17/12*(360-x)) , (360-x)*17/12);
+      }
+
+      //Setting the position
+      translate(intChangeToPlane(intLocationX, 'x'), intChangeToPlane(intLocationY, 'y'));
+
+      //Rotating
+      rotate(radians(x+360/intPetals));
+
+      //Drawing petals
+      ellipse(0, 4*intSize/80*3, 8*intSize/5/intPetals, 4*intSize/10);
+      popMatrix();
+    }
+    //Drawing the center
+    stroke(0,0,0);
+    fill(0,0,0);
+    pushMatrix();
+    translate(intChangeToPlane(intLocationX, 'x'), intChangeToPlane(intLocationY, 'y'));
+    ellipse(0, 0, 24*intSize/80, 24*intSize/80);
+    popMatrix();
+  }
+
+  //I don't like the java co-ordinates
+  public int intChangeToPlane(int intCordsIn, char charCordType){
+    if(charCordType == 'x'){
+      return((screenSize/2 + intCordsIn));
+    }
+    else{
+      return((screenSize/2 - intCordsIn));
+    }
+  }
 }
